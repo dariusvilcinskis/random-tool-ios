a92feb64a6801f9958f864f313827f618fed4c5e
Darius <darius.vilcinskis@live.com>
2017-10-31 16:18:54 +0200

    Initial Commit

---
+++ random-tool-ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  random-tool-ios
+//
+//  Created by Darius Vilcinskis on 31/10/2017.
+//  Copyright © 2017 Darius Vilcinskis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ random-tool-iosTests/random_tool_iosTests.swift
@@ -0,0 +1,36 @@
+//
+//  random_tool_iosTests.swift
+//  random-tool-iosTests
+//
+//  Created by Darius Vilcinskis on 31/10/2017.
+//  Copyright © 2017 Darius Vilcinskis. All rights reserved.
+//
+
+import XCTest
+@testable import random_tool_ios
+
+class random_tool_iosTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
